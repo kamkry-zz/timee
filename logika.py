2e8a54054c922d311efe999104e7d0ce01268430
kamkr <kamkry18@gmail.com>
2020-08-29 18:59:42 +0200

    --[30.08.2020 | 18:06] Added module to transform pseudo-random value based on time of exectiion to vale in range 1 to 6.
    Created by Kamil Krysiak

--- logika.py
+++ logika.py
@@ -1,55 +1,77 @@
 # Logika w Pythonie - generator liczb pseudolosowych i analiza czasu ich dopasowywania
 
+# Biblioteka umożliwiająca pracę z wieloma wątkami
+
 import random
 
 # Biblioteka do generowanie liczb pseudolosowych
 
 import time
 # Biblioteka operacji na czasie rzeczywistym
-a = random.randint(1, 99)
-b = random.randint(100,99999)
-test_logiczny = True
-c = 4
-generator = random.randint(a, b)
-przebiegi_programu = []
-d = 1
-for i in range(100):
-    przebiegi_programu.append(d)
-    d += 1
-iterator = [1]
-pamiec_czasu = []
-test_liczbowy = 0
+import statistics
+# Biblioteka do uśredniania
+histogram = []
+powtorzenia = []
 
-# ***
-test_liczbowy = 0
-for x in przebiegi_programu:
-    czas = time.process_time()
-    for x in iterator:
+j = 1
+for j in range(10):
+    powtorzenia.append(j)
 
-        if(generator == test_liczbowy):
-            print('Logika = True')
-            print(str(test_liczbowy))
-            print(x)
-            koniec_czasu = time.process_time() - czas
-            print(str(koniec_czasu))
-            pamiec_czasu.append(koniec_czasu)
-            test_liczbowy = 0
+    #kostka = [1, 2, 3, 4, 5, 6]     # 6 pól kostki
+    a = random.randint(1, 9)        # losowanie liczby 1-9 jako początku przedziału liczb do losowania w generatorze
+    b = random.randint(100,99999)   # j.w. tylko dla końca przedziału
+    test_logiczny = True            # wartość początkowa zmiennej zakańczającej działanie generatora
+    #c = 4                          # nie używane
+    generator = random.randint(a, b)
+    przebiegi_programu = []
+    d = 1
+    for i in range(10):             # ilość przebiegów procesu losowania
+        przebiegi_programu.append(d)
+        d += 1
+    iterator = [1]
+    pamiec_czasu = []
 
-            break
+    # ***
 
-        else:
+    test_liczbowy = 0
+    for x in przebiegi_programu:
+        czas = time.process_time()
+        for x in iterator:
 
-            print('Logika = False')
-            test_liczbowy += 1
-            iterator.append(generator)
+            if(generator == test_liczbowy):
+                print('Logika = True')
+                print(str(test_liczbowy))
+                print(x)
+                koniec_czasu = time.process_time() - czas
+                print(str(koniec_czasu))
+                pamiec_czasu.append(koniec_czasu)
+                test_liczbowy = 0
+
+                break
+
+            else:
+
+                print('Logika = False')
+                test_liczbowy += 1
+                iterator.append(generator)
 
 
-print('*************************************************************')
-print('*************************************************************')
-print('*************************************************************')
-print('$$$$$$$$$$$$$$$$$   Koniec symulacji   $$$$$$$$$$$$$$$$$$$$$$')
-print('*************************************************************')
-print('*************************************************************')
-print('*************************************************************')
-print(pamiec_czasu)
-print('Ilośc przebiegów tym razem to: ' + str(max(przebiegi_programu)))
+    print('*************************************************************')
+    print('*************************************************************')
+    print('*************************************************************')
+    print('$$$$$$$$$$$$$$$$$   Koniec symulacji   $$$$$$$$$$$$$$$$$$$$$$')
+    print('*************************************************************')
+    print('*************************************************************')
+    print('*************************************************************')
+    #print(pamiec_czasu)
+    liczba_pseudolosowa = statistics.mean(pamiec_czasu)
+    print('Liczba pseudolosowa: ' + str(liczba_pseudolosowa))
+    random.seed(liczba_pseudolosowa)
+    liczba_oczek = random.randint(1, 6)
+    print('Liczba oczek: ' + str(liczba_oczek))
+    histogram.append(liczba_oczek)
+    #print('Ilośc przebiegów tym razem to: ' + str(max(przebiegi_programu)))
+
+    j += 1
+
+print(histogram)
